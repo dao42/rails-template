7645ce25ecd068f3ca5265e1da235ee3d5de9026
yafeilee <lyfi2003@gmail.com>
2019-08-22 20:59:44 +0800

    adjust body_class

--- files/app/helpers/application_helper.rb
+++ files/app/helpers/application_helper.rb
@@ -2,12 +2,22 @@ module ApplicationHelper
   # Generate `{controller}-{action}-page` class for body element
   def body_class
     path = controller_path.tr('/_', '-')
+    action_name_map = {
+      index: 'index',
+      new: 'edit',
+      edit: 'edit',
+      update: 'edit',
+      patch: 'edit',
+      create: 'edit',
+      destory: 'index'
+    }
+    mapped_action_name = action_name_map[action_name.to_sym] || action_name
     body_class_page =
       if controller.is_a?(HighVoltage::StaticPage) && params.key?(:id) && params[:id] !~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
         id_name = params[:id].tr('_', '-') + '-page'
         format('%s-%s-%s', path, action_name, id_name)
       else
-        format('%s-%s-page', path, action_name)
+        format('%s-%s-page', path, mapped_action_name)
       end
 
     body_class_page
