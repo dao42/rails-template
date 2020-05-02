977a7ffaf6ba055d22c8ff848556e29df2466720
Viktor Schmidt <viktor.schmidt@webedia-group.com>
2020-05-02 13:56:23 +0200

    Add Bullet to kill N+1 queries and eager loading

--- composer.rb
+++ composer.rb
@@ -288,6 +288,29 @@ gem_group :development do
   gem 'bundler-audit', require: false
 end
 
+say 'Applying Bullet tool...'
+gem_group :development do
+  gem 'bullet'
+end
+after_bundle do
+  inject_into_file 'config/environments/development.rb', after: "config.file_watcher = ActiveSupport::EventedFileUpdateChecker\n" do <<-EOF
+
+  # watch your queries while you develop your application and notify you
+  # when you should add eager loading (N+1 queries),
+  # when you use eager loading that is not necessary - and
+  # when you should use counter caching
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+    Bullet.rails_logger = false
+    Bullet.bullet_logger = true
+    Bullet.alert = false
+    Bullet.add_footer = false
+  end
+  EOF
+  end
+end
+
 get_remote 'README.md'
 get_remote 'ackrc', '.ackrc'
 get_remote 'bin/setup'
