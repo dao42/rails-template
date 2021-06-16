8e98978e58d77a23a609594153a471a3717c9ef3
windy <yafei@dao42.com>
2021-06-16 18:53:13 +0800

    fix static page class

--- files/app/helpers/application_helper.rb
+++ files/app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
     body_class_page =
       if controller.is_a?(HighVoltage::StaticPage) && params.key?(:id) && params[:id] !~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
         id_name = params[:id].tr('_', '-') + '-page'
-        format('%s-%s-%s', path, action_name, id_name)
+        format('%s-%s', 'pages', id_name)
       else
         format('%s-%s-page', path, mapped_action_name)
       end
